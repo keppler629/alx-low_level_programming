3aa31641ec6f510dd5b58841e7f75b4a0669927e
keppler629 <fsiwale@gmail.com>
2023-01-01 20:21:35 -0800

    6-print_numberz.c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits in base 10
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int c;
+
+	while
+		(c < 10)
+		{
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
